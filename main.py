b2d47f75d7b604b5f8cbfdcffea64d1dc84a160b
Mihai <mihaitg19@gmail.com>
2024-10-10 00:15:47 +0300

    added the base code for the 2nd menu

--- main.py
+++ main.py
@@ -46,16 +46,75 @@ def firstMenu():
             elif acc ==3:
                 print('No products available at te moment')
             else:
-                print('Invalid option, input 1, 2 or 3')
-                break
+                print('Invalid option, input 1, 2 or 3 or 9 to exit')
         except ValueError:
-                print('Invalid option, input 1, 2 or 3')
+                print('Invalid option, input 1, 2 or 3 or 3 or 9 to exit')
 
 welcome = firstMenu()
 
-if isinstance(welcome, tuple):
-    if welcome[0] == True:                           #welcome[0] = loggedIn
-        control=Account()
-        admin=control.checkAdmin(welcome[1])         #welcome[1] = email
-        if admin == 1:
-            print(f'Welcome back admin')
+def secondMenu():
+    check=input('Do you wanna open the menu ? Type yes or no:  ')
+    control=Account()
+    admin=control.checkAdmin(welcome[1])         #welcome[1] = email
+
+    if check == 'yes' or 'Yes':
+        # userExit=False
+        while True:
+            try:
+                if admin == 1:
+                    acc=input('---------------'+
+                            '\nInput a number:'+
+                            '\n1.Product list'+
+                            '\n2.Product details'+
+                            '\n3.Place an order'+
+                            '\n4.Change an order'+
+                            '\n5.Change order info'+
+                            '\n6.Return'+
+                            '\n7.Complaints'+
+                            '\n8.Cancel an order'+
+                            '\n9.Exit'+
+                            '\n10.Add Product'+
+                            '\n---------------'+
+                            '\n')
+                    if int(acc) == 10:
+                        print("Ok let's add some products")
+                else:
+                    acc=input('---------------'+
+                            '\nInput a number:'+
+                            '\n1.Product list'+
+                            '\n2.Product details'+
+                            '\n3.Place an order'+
+                            '\n4.Change an order'+
+                            '\n5.Change order info'+
+                            '\n6.Return'+
+                            '\n7.Complaints'+
+                            '\n8.Cancel an order'+
+                            '\n9.Exit'+
+                            '\n---------------'+
+                            '\n')
+                acc = int(acc)
+                if acc == 1:
+                    print('No products available at te moment')
+                elif acc == 2:
+                    print('No products available at te moment')
+                elif acc == 3:
+                    print('Order closed at the moment')
+                elif acc == 4:
+                    print('Order closed at the moment')
+                elif acc == 5:
+                    print('Order closed at the moment')
+                elif acc == 6:
+                    print('Order closed at the moment')
+                elif acc == 7:
+                    print('No products available at te moment')
+                elif acc == 8:
+                    print('Order closed at the moment')
+                elif acc == 9:
+                    return
+                else:
+                    print('Invalid option')
+            except ValueError:
+                    print('Invalid option, input one of the numbers that is shown in the menu')
+
+if welcome[0] == True:                           #welcome[0] = loggedIn
+    secondMenu()
